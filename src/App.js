7397615048a98467a730645146ee0eb7a7e9ee27
Gautam Bhambri <gautambhambri77@gmail.com>
2021-01-12 15:55:00 +0530

    Latest Update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import './App.css'; 
+import './App.css';   
 import WebcamCapture from './Components/WebcamCapture';
 import {
   BrowserRouter as Router,
